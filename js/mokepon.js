1eb148f897fa2ece9e7e989229d3765bd65cdf6f
David Castellanos <davidalejandrocabeza@gmail.com>
2025-12-31 09:00:38 -0500

    Función que modifíca el DOM y nos indica la mascota seleccionada

--- js/mokepon.js
+++ js/mokepon.js
@@ -6,12 +6,17 @@ botonMascota.addEventListener('click', ()=>{
     let inputCapipepo = document.getElementById('capipepo')
     let inputRatigueya = document.getElementById('ratigueya')
 
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
+
     if(inputHipodoge.checked){
         alert("Mascota hipodoge")
+        spanMascotaJugador.innerHTML = 'Hipodoge'
     }else if(inputCapipepo.checked){
         alert("Mascota capipepo")
+        spanMascotaJugador.innerHTML = 'Capipepo'
     }else if(inputRatigueya.checked){
         alert("Mascota ratigueya")
+        spanMascotaJugador.innerHTML = 'Ratigueya'
     }else{
         alert("Selecciona una mascota")
     }
